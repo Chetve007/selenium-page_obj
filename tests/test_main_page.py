b4cfd34263e323c79484181c2eb059e71863671b
chetve007 <chetve007@mail.ru>
2021-05-30 18:49:03 +0300

    - transitions between pages (2 approaches)

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -1,13 +1,16 @@
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 LINK = "http://selenium1py.pythonanywhere.com/"
-# LINK = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
 
 
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, LINK)
     page.open()
+    # login_page = page.go_to_login_page()  # 1 approach
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
