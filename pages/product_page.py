5a92377e42604c6768180de6349fdbc6b66ee7b6
chetve007 <chetve007@mail.ru>
2021-06-03 20:36:08 +0300

    - refactor product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -7,12 +7,18 @@ class ProductPage(BasePage):
     def add_product_to_basket(self):
         self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
 
-    def should_be_book_name_the_same_in_basket(self):
-        book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+    def get_book_name(self):
+        return self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
+
+    def get_price(self):
+        return self.browser.find_element(*ProductPageLocators.PRICE).text
+
+    def should_be_book_name_the_same_in_basket(self, book_name):
+        # book_name = self.browser.find_element(*ProductPageLocators.BOOK_NAME).text
         book_name_in_basket = self.browser.find_element(*ProductPageLocators.BOOK_NAME_IN_BASKET).text
         assert book_name == book_name_in_basket
 
-    def should_be_price_the_same_in_basket(self):
-        price = self.browser.find_element(*ProductPageLocators.PRICE).text
+    def should_be_price_the_same_in_basket(self, price):
+        # price = self.browser.find_element(*ProductPageLocators.PRICE).text
         price_in_basket = self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET).text
         assert price == price_in_basket
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,6 +1,7 @@
 from pages.product_page import ProductPage
 
-LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+# LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -8,5 +9,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
-    page.should_be_book_name_the_same_in_basket()
-    page.should_be_price_the_same_in_basket()
+    book_name = page.get_book_name()
+    price = page.get_price()
+    page.should_be_book_name_the_same_in_basket(book_name)
+    page.should_be_price_the_same_in_basket(price)
