be8c2ae53d04f121fa3e06a4b55c6be4a92070cd
chetve007 <chetve007@mail.ru>
2021-06-17 21:26:44 +0300

    - link to constant NEW_LINK

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -6,6 +6,7 @@ from pages.product_page import ProductPage
 # LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
 LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 LINK_WITHOUT_PROMO = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+NEW_LINK = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -66,14 +67,14 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = NEW_LINK
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = NEW_LINK
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
