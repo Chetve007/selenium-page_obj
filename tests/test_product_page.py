7efbd16686fe04f0a46313c78a1a37a797ff173a
chetve007 <chetve007@mail.ru>
2021-06-19 14:45:50 +0300

    - add test_guest_cant_see_product_in_basket_opened_from_product_page in test_product_page.py

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
 
@@ -7,6 +8,7 @@ from pages.product_page import ProductPage
 LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
 LINK_WITHOUT_PROMO = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 NEW_LINK = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+LINK_BE = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -67,16 +69,24 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = NEW_LINK
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, NEW_LINK)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = NEW_LINK
-    page = ProductPage(browser, link)
+    page = ProductPage(browser, NEW_LINK)
     page.open()
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    page = ProductPage(browser, LINK_BE)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_basket_page()
+    basket_page.basket_should_be_empty()
+    basket_page.should_not_be_goods_in_basket()
