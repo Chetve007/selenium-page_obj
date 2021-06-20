731e192628ffc7b88b18f89bbc883ccf4b00a1bc
chetve007 <chetve007@mail.ru>
2021-06-20 17:05:54 +0300

    - pytest.param in circle

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -24,20 +24,11 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_price_the_same_in_basket(price)
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  pytest.param(
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                      marks=pytest.mark.xfail
-                                  ),
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket_params(browser, link):
+@pytest.mark.parametrize('promo',
+                         [pytest.param(item, marks=pytest.mark.xfail(item == 7, reason='Bad promo'))
+                          for item in range(10)])
+def test_guest_can_add_product_to_basket_params(browser, promo):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
